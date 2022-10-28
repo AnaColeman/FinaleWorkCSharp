f88d589b592c647563cb0cce2cff613f426f01f9
Ana <anastasia50192iv@gmail.com>
2022-10-28 15:44:56 +0300

    Added 1 for

---
+++ FinaleWork/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу, которая из сформированного массива строк содержащих 
+// символы и числа создать массив из строк, в который попадут только символы, не являющиеця цифрами.
+
+Console.Clear();
+char[] elements = new char[10];
+string alfabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+for (int i = 0; i < elements.Length; i++)
+{
+    int number = new Random().Next(1, 33);
+    string number2;
+    if (i % 2 == 0)
+    {
+        number2 = Convert.ToString(alfabet[number]);
+        elements[i] = Convert.ToChar(number2);
+        Console.Write(elements[i] + ",");
+    }
+    else
+    {
+        number = number % 10;
+        number2 = Convert.ToString(number);
+        elements[i] = Convert.ToChar(number2);
+        Console.Write(elements[i] + ",");
+    }
+}
+Console.WriteLine();
+
